86b2b422b60887502a5c270fae4572c384094064
Srajal Sahu <srajalsahu12@gamil.com>
2024-07-31 13:41:44 +0530

    Add comments

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,63 +1,85 @@
+'''
+-> This module is responsible for collecting and loading the data from the source (e.g., CSV files).
+-> It reads the data into a DataFrame, performs train-test splits, and saves the raw, training, and test data to specified paths.
+-> Logging is used to track the data ingestion process and capture any errors that may occur.
+'''
 import os
 import sys
-from src.exception import CustomException
-from src.logger import logging
 import pandas as pd
-
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
-from src.components.data_transformation import DataTransformation
-from src.components.data_transformation import DataTransformationConfig
+from src.exception import CustomException
+from src.logger import logging
 
-from src.components.model_trainer import ModelTrainerConfig
-from src.components.model_trainer import ModelTrainer
-
+# Define a data class for the data ingestion configuration
 @dataclass
 class DataIngestionConfig:
-    train_data_path: str=os.path.join('artifacts',"train.csv")
-    test_data_path: str=os.path.join('artifacts',"test.csv")
-    raw_data_path: str=os.path.join('artifacts',"data.csv")
+    # Paths to save the train, test, and raw data
+    train_data_path: str = os.path.join('artifacts', "train.csv")
+    test_data_path: str = os.path.join('artifacts', "test.csv")
+    raw_data_path: str = os.path.join('artifacts', "data.csv")
 
+# Define the DataIngestion class
 class DataIngestion:
     def __init__(self):
-        self.ingestion_config=DataIngestionConfig()
+        # Initialize the data ingestion configuration
+        self.ingestion_config = DataIngestionConfig()
 
     def initiate_data_ingestion(self):
+        """
+        This function is responsible for data ingestion
+        """
         logging.info("Entered the data ingestion method or component")
         try:
+            # Print the current working directory
             print("Current Working Directory:", os.getcwd())
-            # df=pd.read_csv(r'notebook\data\stud.csv')
+            
+            # Read the dataset into a DataFrame
             df = pd.read_csv(r'..\..\notebook\data\stud.csv')
             logging.info('Read the dataset as dataframe')
 
-            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+            # Create the directory if it does not exist
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
 
-            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+            # Save the raw data to the specified path
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
 
             logging.info("Train test split initiated")
-            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
+            
+            # Split the data into train and test sets
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
 
-            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            # Save the train and test sets to the specified paths
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
 
-            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+            logging.info("Ingestion of the data is completed")
 
-            logging.info("Inmgestion of the data iss completed")
-
-            return(
+            # Return the paths to the train and test sets
+            return (
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path
-
             )
         except Exception as e:
-            raise CustomException(e,sys)
-        
-if __name__=="__main__":
-    obj=DataIngestion()
-    train_data,test_data=obj.initiate_data_ingestion()
+            # Raise a custom exception if any error occurs
+            raise CustomException(e, sys)
 
-    data_transformation=DataTransformation()
-    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+# If this script is run directly, initiate the data ingestion process
+if __name__ == "__main__":
+    # Create an instance of the DataIngestion class
+    obj = DataIngestion()
+    # Start the data ingestion process and get the train and test data paths
+    train_data, test_data = obj.initiate_data_ingestion()
 
-    modeltrainer=ModelTrainer()
-    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
+    # Import DataTransformation class for transforming the data
+    from src.components.data_transformation import DataTransformation
+    data_transformation = DataTransformation()
+    # Start the data transformation process
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
+
+    # Import ModelTrainer class for training the model
+    from src.components.model_trainer import ModelTrainer
+    modeltrainer = ModelTrainer()
+    # Start the model training process and print the evaluation score
+    print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -1,7 +1,12 @@
-import os
-import sys
-from dataclasses import dataclass
+'''
+-> This module is responsible for training various machine learning models on the preprocessed data.
+-> It evaluates different models using techniques like GridSearchCV to find the best hyperparameters.
+-> The performance of each model is recorded, and the best-performing model is saved for future predictions.
+'''
 
+import os  #Provides functions to interact with the operating system.
+import sys  #Provides access to system-specific parameters and functions.
+from dataclasses import dataclass # A decorator to automatically generate special methods like __init__ and __repr__ for classes.
 
 from sklearn.ensemble import (
     AdaBoostRegressor,
@@ -10,33 +15,38 @@ from sklearn.ensemble import (
 )
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
-from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
 from xgboost import XGBRegressor
 
 from src.exception import CustomException
 from src.logger import logging
+from src.utils import save_object, evaluate_models
 
-from src.utils import save_object,evaluate_models
-
+# Define a data class for the model trainer configuration
 @dataclass
 class ModelTrainerConfig:
-    trained_model_file_path=os.path.join("artifacts","model.pkl")
+    # Path to save the trained model as a pickle file
+    trained_model_file_path = os.path.join("artifacts", "model.pkl")
 
+# Define the ModelTrainer class
 class ModelTrainer:
     def __init__(self):
-        self.model_trainer_config=ModelTrainerConfig()
+        # Initialize the model trainer configuration
+        self.model_trainer_config = ModelTrainerConfig()
 
-
-    def initiate_model_trainer(self,train_array,test_array):
+    def initiate_model_trainer(self, train_array, test_array):
         try:
             logging.info("Split training and test input data")
-            X_train,y_train,X_test,y_test=(
-                train_array[:,:-1],
-                train_array[:,-1],
-                test_array[:,:-1],
-                test_array[:,-1]
+
+            # Split the train and test arrays into input features (X) and target feature (y)
+            X_train, y_train, X_test, y_test = (
+                train_array[:, :-1],  # All columns except the last one for training data
+                train_array[:, -1],   # The last column for training data
+                test_array[:, :-1],   # All columns except the last one for testing data
+                test_array[:, -1]     # The last column for testing data
             )
+
+            # Define a dictionary of regression models to be evaluated
             models = {
                 "Random Forest": RandomForestRegressor(),
                 "Decision Tree": DecisionTreeRegressor(),
@@ -45,66 +55,65 @@ class ModelTrainer:
                 "XGBRegressor": XGBRegressor(),
                 "AdaBoost Regressor": AdaBoostRegressor(),
             }
-            params={
+
+            # Define hyperparameters for each model
+            params = {
                 "Decision Tree": {
-                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
-                    # 'splitter':['best','random'],
-                    # 'max_features':['sqrt','log2'],
+                    'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
                 },
-                "Random Forest":{
-                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
-                 
-                    # 'max_features':['sqrt','log2',None],
-                    'n_estimators': [8,16,32,64,128,256]
+                "Random Forest": {
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
                 },
-                "Gradient Boosting":{
-                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
-                    'learning_rate':[.1,.01,.05,.001],
-                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
-                    # 'criterion':['squared_error', 'friedman_mse'],
-                    # 'max_features':['auto','sqrt','log2'],
-                    'n_estimators': [8,16,32,64,128,256]
+                "Gradient Boosting": {
+                    'learning_rate': [.1, .01, .05, .001],
+                    'subsample': [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
                 },
-                "Linear Regression":{},
-                "XGBRegressor":{
-                    'learning_rate':[.1,.01,.05,.001],
-                    'n_estimators': [8,16,32,64,128,256]
+                "Linear Regression": {},
+                "XGBRegressor": {
+                    'learning_rate': [.1, .01, .05, .001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
                 },
-                "AdaBoost Regressor":{
-                    'learning_rate':[.1,.01,0.5,.001],
-                    # 'loss':['linear','square','exponential'],
-                    'n_estimators': [8,16,32,64,128,256]
+                "AdaBoost Regressor": {
+                    'learning_rate': [.1, .01, 0.5, .001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
                 }
-                
             }
 
-            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
-                                             models=models,param=params)
-            
-            ## To get best model score from dict
+            # Evaluate models using the custom evaluate_models function
+            model_report: dict = evaluate_models(
+                X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test,
+                models=models, param=params
+            )
+
+            # Get the best model score from the evaluation report
             best_model_score = max(sorted(model_report.values()))
 
-            ## To get best model name from dict
-
+            # Get the best model name from the evaluation report
             best_model_name = list(model_report.keys())[
                 list(model_report.values()).index(best_model_score)
             ]
             best_model = models[best_model_name]
 
-            if best_model_score<0.6:
+            # Check if the best model's score is above the threshold (0.6)
+            if best_model_score < 0.6:
                 raise CustomException("No best model found")
+
             logging.info(f"Best found model on both training and testing dataset")
 
+            # Save the best model to disk
             save_object(
                 file_path=self.model_trainer_config.trained_model_file_path,
                 obj=best_model
             )
 
-            predicted=best_model.predict(X_test)
+            # Use the best model to make predictions on the test set
+            predicted = best_model.predict(X_test)
 
+            # Calculate the R² score to evaluate the model's performance on the test set
             r2_square = r2_score(y_test, predicted)
             return r2_square
-            
-            
+
         except Exception as e:
-            raise CustomException(e,sys)
+            # Raise a custom exception if any error occurs
+            raise CustomException(e, sys)
